51475f2b691980f77049ed99ec1b74e5403e895b
Lance Woolie <lancewoolie@gmail.com>
2025-10-21 13:14:30 -0500

    Add loadPromos function to display promo messages

---
+++ js/promos.js
@@ -0,0 +1,6 @@
+function loadPromos() {
+    const container = document.getElementById('promos-list');
+    container.innerHTML = '<p>Deals with Tesla/Amazon referral links will appear here.</p>';
+    // TODO: RSS fetch for Tesla, Amazon API
+    console.log('Promos loaded (stub)');
+}
