9d03af76f19431a50c8ad4e7b679edee250c307b
Lance Woolie <lancewoolie@gmail.com>
2025-10-21 13:13:40 -0500

    Add loadFlights function to display flight data

---
+++ js/flights.js
@@ -0,0 +1,6 @@
+function loadFlights() {
+    const container = document.getElementById('flights-list');
+    container.innerHTML = '<p>Flights data for BTR/MSY airports will load here.</p>';
+    // TODO: fetch AviationStack API
+    console.log('Flights loaded (stub)');
+}
