dd620db4d290269fe7ff3b4e630ecca7be541399
Lance Woolie <lancewoolie@gmail.com>
2025-10-21 13:58:19 -0500

    Add TomTom API configuration
    Added configuration for TomTom API key.

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// Super Uber API Config (Replace with real keys; keep private for now)
+const TOMTOM_KEY = '4824d68f-e7ef-4e5c-83ba-d95b5126fbe4';
+
+// Eventbrite will go here next: const EVENTBRITE_TOKEN = 'your_token';
