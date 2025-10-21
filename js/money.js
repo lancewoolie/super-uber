3ab4e0304550d541511103108e7eb737f5e4cacb
Lance Woolie <lancewoolie@gmail.com>
2025-10-21 13:14:01 -0500

    Add loadMoney function to display earnings tracker

---
+++ js/money.js
@@ -0,0 +1,6 @@
+function loadMoney() {
+    const container = document.getElementById('money-dashboard');
+    container.innerHTML = '<p>Mileage & earnings tracker (Uber API connect coming).</p>';
+    // TODO: Manual input form + Uber OAuth
+    console.log('Money loaded (stub)');
+}
